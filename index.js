d62e8adbacc4f05282f7c0e5b221980dafaed593
jerome2710 <info@jeromeanker.nl>
2017-03-09 14:42:44 +0100

    adding basic API routes

--- index.js
+++ index.js
@@ -46,6 +46,56 @@ app.get('/', function(req, res) {
     res.send('Hello! The API is at http://localhost:' + port + '/api');
 });
 
+app.get('/setup', function(req, res) {
+
+    // create a sample user
+    var admin = new User({
+        name: 'admin',
+        password: 'admin',
+        admin: true
+    });
+
+    // save the sample user
+    admin.save(function(err) {
+        if (err) throw err;
+
+        console.log('User saved successfully');
+        res.json({
+            success: 'success',
+            data: {}
+        });
+    });
+});
+
+
+// API ROUTES -------------------
+
+// get an instance of the router for api routes
+var apiRoutes = express.Router();
+
+// TODO: route to authenticate a user (POST http://localhost:8080/api/authenticate)
+
+// TODO: route middleware to verify a token
+
+// route to show a random message (GET http://localhost:8080/api/)
+apiRoutes.get('/', function(req, res) {
+    res.json({ message: 'Welcome to the coolest API on earth!' });
+});
+
+// route to return all users (GET http://localhost:8080/api/users)
+apiRoutes.get('/users', function(req, res) {
+    User.find({}, function(err, users) {
+        res.json(users);
+    });
+});
+
+// apply the routes to our application with the prefix /api
+app.use('/api', apiRoutes);
+
+
+
+
+
 // API ROUTES -------------------
 // we'll get to these in a second
 
